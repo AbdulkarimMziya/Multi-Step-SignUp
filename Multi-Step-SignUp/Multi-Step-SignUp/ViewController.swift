0569ce34ca06b32942dce5d1874ce95ee816d1c6
Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>
2026-04-11 21:00:39 -0700

    feat: initialize Multi-Step-SignUp iOS project

---
+++ Multi-Step-SignUp/Multi-Step-SignUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Multi-Step-SignUp
+//
+//  Created by Abdulkarim Mziya on 2026-04-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
