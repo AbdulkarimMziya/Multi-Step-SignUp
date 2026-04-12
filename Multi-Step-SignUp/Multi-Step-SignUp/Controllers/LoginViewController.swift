e481657cba058ec5acc1b837bde7459d5c48a2b0
Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>
2026-04-11 22:16:47 -0700

    feat: implement programmatic LoginViewController with title and subtitle labels

--- Multi-Step-SignUp/Multi-Step-SignUp/Controllers/LoginViewController.swift
+++ Multi-Step-SignUp/Multi-Step-SignUp/Controllers/LoginViewController.swift
@@ -8,11 +8,57 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    lazy var mainStack: UIStackView = {
+       let stack = UIStackView(arrangedSubviews: [loginTitleLabel,loginSubTitleLabel])
+        stack.axis = .vertical
+        stack.alignment = .center
+        stack.spacing = 8
+        stack.backgroundColor = .secondarySystemBackground
+        
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
+    let loginTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Let's Get Started!"
+        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let loginSubTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Set up your account credentials."
+        label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        addViews()
+        setupConstraints()
+        
     }
+    
+    func addViews() {
+        view.addSubview(mainStack)
+    }
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            mainStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            mainStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            mainStack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+        ])
+    }
+
 
 
 }
