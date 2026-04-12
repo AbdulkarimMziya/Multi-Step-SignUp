0e8786e6b0d368f092f5ce9ab85352030f1767d1
Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>
2026-04-12 15:55:16 -0700

    feat: add self-managed focus styling to CustomTextField

--- Multi-Step-SignUp/Multi-Step-SignUp/Views/CustomTextField.swift
+++ Multi-Step-SignUp/Multi-Step-SignUp/Views/CustomTextField.swift
@@ -21,16 +21,18 @@ class CustomTextField: UIView {
     private let headerLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        label.textColor = .white
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     private let textField: UITextField = {
         let field = UITextField()
+        field.textColor = .white
         field.textAlignment = .natural
         field.font = UIFont.systemFont(ofSize: 16, weight: .regular)
         field.layer.cornerRadius = 8
-        field.layer.borderColor = UIColor.systemRed.cgColor
+        field.layer.borderColor = UIColor.systemGray4.cgColor
         field.layer.borderWidth = 1
         field.clipsToBounds = true
         field.heightAnchor.constraint(equalToConstant: 48).isActive = true
@@ -42,6 +44,9 @@ class CustomTextField: UIView {
         super.init(frame: frame)
         addSubview(mainStack)
             
+        textField.addTarget(self, action: #selector(handleEditingDidBegin), for: .editingDidBegin)
+        textField.addTarget(self, action: #selector(handleEditingDidEnd), for: .editingDidEnd)
+
         NSLayoutConstraint.activate([
             mainStack.topAnchor.constraint(equalTo: topAnchor),
             mainStack.leadingAnchor.constraint(equalTo: leadingAnchor),
@@ -59,12 +64,46 @@ class CustomTextField: UIView {
         textField.placeholder = placeholder
         textField.isSecureTextEntry = isSecure
         
-        // TODO: add icon in textfield
-        let iconImage = UIImage(systemName: iconName)
+        
+        if let iconImage = UIImage(systemName: iconName){
+            textField.leftView = setLeftViewIcon(iconImage)
+        }
         
         // Handle specific keyboard types
         if header.lowercased().contains("email") {
             textField.keyboardType = .emailAddress
         }
     }
+    
+    private func setLeftViewIcon(_ image: UIImage) -> UIView {
+        let iconView = UIImageView(image: image)
+        iconView.tintColor = .white
+        iconView.contentMode = .scaleAspectFit
+        
+        // 1. Create a container to provide padding
+        let containerView = UIView(frame: CGRect(x: 0, y: 0, width: 40, height: 20))
+        
+        // 2. Set the icon frame within that container
+        iconView.frame = CGRect(x: 10, y: 0, width: 20, height: 20)
+        containerView.addSubview(iconView)
+        
+        // 3. IMPORTANT: Tell the textfield to actually show the view
+        textField.leftViewMode = .always
+        
+        return containerView
+    }
+    
+    @objc
+    private func handleEditingDidBegin() {
+        textField.layer.borderColor = UIColor.limeGreen.cgColor // Highlight
+        textField.layer.borderWidth = 2
+    }
+
+    @objc
+    private func handleEditingDidEnd() {
+        textField.layer.borderColor = UIColor.systemGray4.cgColor // Reset
+        textField.layer.borderWidth = 1
+    }
+
+
 }
