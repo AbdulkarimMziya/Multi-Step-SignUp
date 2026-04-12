62d1a45ba04f2b3f41c4de232e1984a23bc17bc3
Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>
2026-04-12 15:50:56 -0700

    feat: create UIColor extension for greyBackground and limeGreen

--- Multi-Step-SignUp/Multi-Step-SignUp/Extensions/UIColor+Extension.swift
+++ Multi-Step-SignUp/Multi-Step-SignUp/Extensions/UIColor+Extension.swift
@@ -5,4 +5,9 @@
 //  Created by Abdulkarim Mziya on 2026-04-12.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    static let greyBackground = UIColor(red: 21/255, green: 21/255, blue: 19/255, alpha: 1)
+    static let limeGreen = UIColor(red: 210/255, green: 240/255, blue: 101/255, alpha: 1)
+}
