9bcfea7834928f150928be9af805ec7155af8cb9
Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>
2026-04-12 22:27:13 -0700

    style: add specific radio button color constants to UIColor extension

--- Multi-Step-SignUp/Multi-Step-SignUp/Extensions/UIColor+Extension.swift
+++ Multi-Step-SignUp/Multi-Step-SignUp/Extensions/UIColor+Extension.swift
@@ -10,4 +10,7 @@ import UIKit
 extension UIColor {
     static let greyBackground = UIColor(red: 21/255, green: 21/255, blue: 19/255, alpha: 1)
     static let limeGreen = UIColor(red: 210/255, green: 240/255, blue: 101/255, alpha: 1)
+    static let radioGreen = UIColor(red: 136/255, green: 163/255, blue: 66/255, alpha: 1)
+    static let radioBlue = UIColor(red: 67/255, green: 9/255, blue: 237/255, alpha: 1)
+    static let radioPink = UIColor(red: 229/255, green: 134/255, blue: 247/255, alpha: 1)
 }
