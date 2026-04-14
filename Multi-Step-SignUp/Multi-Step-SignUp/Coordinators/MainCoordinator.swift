eae70f9b2cdfeb8b794c747ea606f4168a414d2e
Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>
2026-04-14 11:46:22 -0700

    docs: documented on the implementation of the coordinator

--- Multi-Step-SignUp/Multi-Step-SignUp/Coordinators/MainCoordinator.swift
+++ Multi-Step-SignUp/Multi-Step-SignUp/Coordinators/MainCoordinator.swift
@@ -9,26 +9,35 @@ import Foundation
 
 import UIKit
 
+// This protocol defines the basic responsibilities of any Coordinator
+// Coordinators are responsible for handling navigation (NOT ViewControllers)
 protocol Coordinator: AnyObject {
+    // The navigation controller that will handle screen transitions
     var navigationController: UINavigationController { get set }
-    
+
+    // This is the FIRST function that gets called to start the app flow
     func start()
 }
 
 class MainCoordinator: Coordinator {
-    var navigationController: UINavigationController
+    var navigationController: UINavigationController // The navigation controller used to push/pop view controllers
     
+    // Inject navigation controller (Dependency Injection)
     init(navigationController: UINavigationController) {
         self.navigationController = navigationController
     }
     
     func start() {
         let loginVC = LoginViewController()
-        loginVC.coordinator = self
+        // Give the ViewController access to the coordinator
+        loginVC.coordinator = self // This allows the VC to ASK the coordinator to navigate
         loginVC.navigationItem.hidesBackButton = true
+        
+        // Push the first screen WITHOUT animation
         navigationController.pushViewController(loginVC, animated: false)
     }
     
+    // MARK: - Navigation Functions
     func showProfilePage() {
         let profileVC = ProfileViewController()
         profileVC.coordinator = self
